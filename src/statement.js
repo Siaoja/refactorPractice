3fa199a0c0c97150e74d6a15509c18bff99c3fc8
Siaoja <lxj971010>
2020-08-27 14:45:43 +0800

    [refactor practice][spike] refactor: exact method calculateAmount to calculate amount.

--- src/statement.js
+++ src/statement.js
@@ -11,28 +11,7 @@ function statement (invoice, plays) {
   //遍历所有的剧场剧目
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      //悲剧的收费
-      case 'tragedy':
-        thisAmount = 40000;
-        //如果观众人数多于30人，加收费用
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-        //喜剧收费
-      case 'comedy':
-        thisAmount = 30000;
-        //如果观众多于20人，加收费用
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = calculateAmount(play,perf);
     // add volume credits  计算返还客户的积分
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -46,6 +25,32 @@ function statement (invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
+}
+
+function calculateAmount(play,perf){
+  let thisAmount = 0;
+  switch (play.type) {
+      //悲剧的收费
+    case 'tragedy':
+      thisAmount = 40000;
+      //如果观众人数多于30人，加收费用
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+      //喜剧收费
+    case 'comedy':
+      thisAmount = 30000;
+      //如果观众多于20人，加收费用
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
 }
 
 module.exports = {
